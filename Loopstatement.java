046f11673b5e3436f882a9e39bfb9bd15fb2e36d
FrancisDacian <2411837915@qq.com>
2018-01-23 00:14:44 +0800

    Add files via upload

---
+++ Loopstatement.java
@@ -0,0 +1,14 @@
+//dacianyu
+//Loopstatement
+//2018.1.1
+
+public class Loopstatement
+{
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<10;i++)
+		{
+		System.out.println("Hello World!");
+		}
+	}
+}
