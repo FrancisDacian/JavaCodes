85b181f6b05e8ca5d9300ae6ded8ad4d0e615b3c
FrancisDacian <2411837915@qq.com>
2018-01-22 22:49:39 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+//dacianyu
+//first hello world
+// 2018.1.1
+
+//public is only one in a java file
+//Hello:name
+public class Hello
+{
+	//entrance
+	public static void main(String args[])
+	{
+		//run
+		System.out.println("hello!!");
+	}
+}
+
+//javac Hello.java
+//java Hello
+
+//Hello!
