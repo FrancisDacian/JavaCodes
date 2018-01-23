7a5ed5fed4e3a8425b563edb25dad8396171eb3c
FrancisDacian <2411837915@qq.com>
2018-01-23 20:33:18 +0800

    Create override.java

---
+++ inherited-overload-override/override.java
@@ -0,0 +1,40 @@
+package com.dacian;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat cat1 = new Cat();
+		cat1.cry();
+		Dog dog1 = new Dog();
+		dog1.cry();
+	}
+
+}
+
+//override
+/*class Animal
+{
+	int age;
+	String name;
+	public void cry()
+	{
+		System.out.println("im animal,don't know");
+	}
+}
+
+class Cat extends Animal
+{
+	public void cry()
+	{
+		System.out.println("miaomiao");
+	}
+}
+
+class Dog extends Animal
+{
+	public void cry()
+	{
+		System.out.println("wangwang");
+	}
+}*/
